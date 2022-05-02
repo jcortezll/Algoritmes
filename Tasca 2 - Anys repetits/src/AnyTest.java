3dd36196c532d181b778df741805ef837fe8447b
jcortezll <joluenti@gmail.com>
2022-05-02 09:24:20 +0200

    Segon test any

--- Tasca 2 - Anys repetits/src/AnyTest.java
+++ Tasca 2 - Anys repetits/src/AnyTest.java
@@ -9,5 +9,11 @@ class AnyTest {
         int esperat=4;
         Any any= new Any(2015);
         assertEquals(esperat,any.digitsDiferents());
+        Any any2 = new Any(2020);
+        esperat=2;
+        assertEquals(esperat,any2.digitsDiferents());
+        Any any3 = new Any(2022);
+        esperat=2;
+        assertEquals(esperat,any3.digitsDiferents());
     }
 }
